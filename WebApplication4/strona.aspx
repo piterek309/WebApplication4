bb4bc544e7a101b7d04d05e1f16fdb87092a61fb
piterek309 <75033427+piterek309@users.noreply.github.com>
2021-01-31 16:04:22 +0100

    Dodaj pliki projektów.

---
+++ WebApplication4/strona.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="strona.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Podaj swoje imie i nazwisko"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="168px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Height="20px" OnClick="Button1_Click" Text="Potwierdź" Width="80px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/strona.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label1.Text = ("Dziękujemy, Twoje dane zostały zapisane");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection apkazaliczenie = new SqlConnection("Server=tcp:aplikacja62632.database.windows.net,1433;Initial Catalog=Aplikacja62632DB;Persist Security Info=False;User ID=Goliński;Password=Wsb62632;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("EXEC dbo.InsertFullname @Fullname", apkazaliczenie);
+                insert.Parameters.AddWithValue("@Fullname", TextBox1.Text);
+                apkazaliczenie.Open();
+                insert.ExecuteNonQuery();
+                apkazaliczenie.Close();
+                
+                if (IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+        }
+    }
+}
